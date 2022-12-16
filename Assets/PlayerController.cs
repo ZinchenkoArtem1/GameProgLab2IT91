305e15fc217128613fb252e71575a0ee7d0e31df
ArtemZinchenko <Artem.Zinchenko@livevision.com.ua>
2022-12-16 12:24:32 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private float Speed = 6f;
+    private float JumpForce = 150f;
+
+    private bool _isGrounded;
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MovementLogic();
+        JumpLogic();
+    }
+
+    private void MovementLogic()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rb.AddForce(movement * Speed);
+    }
+
+    private void JumpLogic()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (_isGrounded)
+            {
+                _rb.AddForce(Vector3.up * JumpForce);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        IsGroundedUpate(collision, true);
+        IsFinish(collision);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        IsGroundedUpate(collision, false);
+    }
+
+    private void IsGroundedUpate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            _isGrounded = value;
+        }
+    }
+
+    private void IsFinish(Collision collision) 
+    {
+        if(collision.gameObject.tag == ("Finish"))
+        {
+            SceneManager.LoadScene("FinishScene", LoadSceneMode.Single);
+        }
+    }
+}
